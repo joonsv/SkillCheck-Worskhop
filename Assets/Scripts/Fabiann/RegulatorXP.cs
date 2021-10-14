0e589c1ddf1fa187a821b29f50fdd45ae8538f9a
Faab <fabianfransen96@hotmail.com>
2021-10-14 14:07:32 +0200

    Integratie

---
+++ Assets/Scripts/Fabiann/RegulatorXP.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegulatorXP : MonoBehaviour
+{
+    private int xpPoints;
+    private int skillPoints = 0;
+    [SerializeField] private int breadCrumbXP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SkillPoint"))
+        {
+            other.gameObject.SetActive(false);
+            skillPoints += 1;
+        }
+        if (other.gameObject.CompareTag("BreadCrumb"))
+        {
+            other.gameObject.SetActive(false);
+            xpGain(breadCrumbXP);
+        }
+    }
+    public void xpGain(int xpAmount)
+    {
+        xpPoints += xpAmount;
+        //resterende xp gaat verloren nu
+        Debug.Log("xpGain()");
+        Debug.Log("xpPoints: " + xpPoints);
+        Debug.Log("skillPoints: " + skillPoints);
+
+        if (xpPoints >= 10)
+        {
+            xpPoints = 0;
+            skillPoints += 1;
+        }
+    }
+}
+
